505f8a3f94000dc0af04cb5576aa213bcac5e74a
Marci Prescott <mjprescott@me.com>
2025-02-10 16:16:50 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Heading from "./Heading.jsx";
+import Body from "./Body.jsx";
+
+ReactDOM.render(
+  <div>
+    <Heading />
+    <Body />
+  </div>,
+  document.getElementById("root")
+);
